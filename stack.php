b8a6a2f0e4f9b67079428a445a79b4718cf6b83d
Thomas Kolmans <thomas@kolmans.com>
2017-02-27 17:01:22 +0100

    Delete stack.php

--- stack.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!isset($_POST["name"])){
-    select("*");
-}else{
-    select($_POST["name"]);
-}
-function select(){
-     $query .= 'SELECT ' . (func_num_args() == 0 ? '*' : '`' . implode("`, `", func_get_args()) . '`').' FROM table ';
-}
